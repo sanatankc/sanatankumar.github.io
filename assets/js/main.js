fd35559bc6f7793cddce9dac856619b5e3091faa
Sanatan <sanatan.iit@gmail.com>
2017-02-03 06:52:56 +0530

    v2 of js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
     $( ".mobile-nav-toggle" ).click(function() {
     $('.mobile-nav-toggle').toggleClass('open');
-    $('.mobile-nav').toggleClass('open');
+    $('nav').toggleClass('open');
 });
 });
